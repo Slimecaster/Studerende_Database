2e787646388b616f7044ebee58e36bc6e9aafaa0
Slimecaster <nd070503@gmail.com>
2023-11-15 14:23:47 +0100

    Initial commit

---
+++ src/DbSql.java
@@ -0,0 +1,104 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class DbSql {
+    private Connection connection;
+    private Statement stmt;
+    private Statement stmt1;
+
+    DbSql() {
+        connection = null;
+        stmt = null;
+        try {
+            String url = "jdbc:sqlite:C:/Users/nd070/OneDrive/Dokumenter/StuderendeDataBase.db";
+            connection = DriverManager.getConnection(url);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void opretStuderende(Studerende s) {
+        try {
+            String sql = "insert into Studerende (stdnr,fnavn,enavn,adresse,postnr,mobil,klasse)";
+            sql += "values (" + String.valueOf(s.getStdnr()) + ",'" + s.getFnavn() + "','" + s.getEnavn() + "','" + s.getAdresse() + "','" + s.getPostnr() + "','" + s.getMobil() + "','" + s.getKlasse() + "')";
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            System.out.println("Connection to SQLite has been established.");
+
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+    public void opretFag(Fag f) {
+        try {
+            String sql = "insert into Fag (fagNr,fagNavn)";
+            sql += "values (" + String.valueOf(f.getFagnr()) + ",'" + f.getFagnavn() + "')";
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            System.out.println("Connection to SQLite has been established.");
+
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void tilmeldStudereneTilFag(int stdnr, int fagnr, int kar) {
+        try {
+            String sql = "insert into Studfag(stdnr,fagNr,kar) values(";
+            sql += String.valueOf(stdnr) + "," + String.valueOf(fagnr) + "," + String.valueOf(kar) + ")";
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            System.out.println("Connection to SQLite has been established.");
+
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+        public void alleStuderende () {
+            try {
+                String sql = "select * from Studerende";
+                Statement stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                System.out.println("Connection to SQLite has been established.");
+                while (rs.next()) {
+                    System.out.println(rs.getInt("stdnr"));
+                    System.out.println(rs.getString("fnavn"));
+                    System.out.println(rs.getString("enavn"));
+                    System.out.println(rs.getString("adresse"));
+                    System.out.println(rs.getString("postnr"));
+                    System.out.println(rs.getString("mobil"));
+                    System.out.println(rs.getString("klasse"));
+                }
+                stmt.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+        }
+    public void alleFag(){
+        try {
+            String sql = "select * from Fag";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("Connection to SQLite has been established.");
+            while (rs.next()) {
+                System.out.println(rs.getInt("fagNr"));
+                System.out.println(rs.getString("fagNavn"));
+            }
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+    }
---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        DbSql db = new DbSql();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("1. Opret Studerende");
+        System.out.println("2. Opret fag");
+        System.out.println("3. Tilmeld studerende til fag");
+        System.out.println("4. Frameld studerende til fag");
+        System.out.println("5. Udskriv alle studerende");
+        System.out.println("6. Udskriv alle fag");
+        System.out.println("7. Søg oplysninger om en studerende");
+        System.out.println("8. Søg oplysninger om et fag");
+        System.out.println("Indtast dit valg: ");
+        int valg = input.nextInt();
+        switch (valg) {
+            case 1:
+                Studerende s = new Studerende(15, "Mikkel", "Jensen", "Holbækvej 13", "2200", "11 22 33 55", 'a');
+                db.opretStuderende(s);
+                break;
+            case 2:
+                Fag f1= new Fag(2,"Java");
+                db.opretFag(f1);
+                break;
+            case 3:
+                db.tilmeldStudereneTilFag(1,2,12);
+                break;
+            case 4:
+                break;
+            case 5:
+                db.alleStuderende();
+                break;
+            case 6:
+                db.alleFag();
+                break;
+        }
+    }
+}
